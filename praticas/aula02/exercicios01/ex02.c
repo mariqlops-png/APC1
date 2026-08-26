c2d43e274ee0356827634576589bcfe90e4378e0
unknown <2622130019@ACADEMICO.IESB.BR>
2026-08-26 09:58:23 -0300

    enviando a pratica 3

---
+++ praticas/aula02/exercicios01/ex02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int idade;
+float altura;
+char inicial;
+int aprovacao;
+
+int main() {
+    idade = 18;
+    altura = 1.57;
+    inicial = 'M';
+    aprovacao = 1;
+   
+    printf("idade: %d\n",idade);
+    printf("Altura: %f\n", altura);
+    printf("inicial do nome: %c\n", inicial);
+    printf("Aprovacao %d\n", aprovacao);
+    
+}
